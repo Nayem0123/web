e5adf61a754ff94aca84960d8e892bfe0717b87e
siam0123 <34501603+siam0123@users.noreply.github.com>
2017-12-29 00:24:05 +0600

    Delete image-banner.php

--- app/view/image-banner.php
+++
@@ -1,9 +0,0 @@
-<fieldset>
-    <table width="100%" cellpadding="0" cellspacing="0">
-        <tr>
-            <td>
-                <a href=<?php echo APP_ROOT; ?>><img width="100%" height="250px" src=<?php echo APP_ROOT."/asset/images/image.jpg"; ?>></a>
-            </td>
-        </tr>
-    </table>
-</fieldset>
