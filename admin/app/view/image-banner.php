08dd45d862af273aafba391f142588ff9ff96f1f
Nayem0123 <33677894+Nayem0123@users.noreply.github.com>
2017-12-29 00:09:16 +0600

    Add files via upload

---
+++ admin/app/view/image-banner.php
@@ -0,0 +1,9 @@
+<fieldset>
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <a href=<?php echo APP_ROOT; ?>><img width="100%" height="250px" src=<?php echo APP_ROOT."/asset/images/image.jpg"; ?>></a>
+            </td>
+        </tr>
+    </table>
+</fieldset>
