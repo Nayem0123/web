8ee7fa2f0895ef82dbc7deae8964dd4290b59d6c
Nayem0123 <33677894+Nayem0123@users.noreply.github.com>
2017-12-28 08:19:41 +0600

    Add files via upload

---
+++ admin/app/view/left-panel.php
@@ -0,0 +1,13 @@
+<td width="300" valign="top">
+    <b>Account</b><hr/>
+    <ul>
+        <li><a href=<?php echo APP_ROOT."/admin/?home_order-list"; ?>>Home</a></li>
+        <li><a href=<?php echo APP_ROOT."/admin/?home_search"; ?>>Search</a></li>
+        <li><a href=<?php echo APP_ROOT."/admin/?home_all-product"; ?>>see all product</a></li>
+        <li><a href=<?php echo APP_ROOT."/admin/?home_category"; ?>>Category</a></li>
+ 		<li><a href=<?php echo APP_ROOT."/admin/?home_company"; ?>>Company</a></li>
+        <li><a href=<?php echo APP_ROOT."/admin/?account_update-profile"; ?>>Update profile</a></li>
+        <!--li><a href=<?php echo APP_ROOT."/admin/?home_categories"; ?>>Add Quantity</a></li-->
+        <li><a href=<?php echo APP_ROOT."/admin/?home_product"; ?>>Add New Product</a></li>
+    </ul>  
+</td>
